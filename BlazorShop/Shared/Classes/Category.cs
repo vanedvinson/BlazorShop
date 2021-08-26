eff97599056a35cdefd5f30676fbd53656ee7998
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-08-26 16:38:34 +0200

    Project + Models Created

---
+++ BlazorShop/Shared/Classes/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Shared.Classes
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public string Icon { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ BlazorShop/Shared/Classes/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Shared.Classes
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Descripton { get; set; }
+        public string ImageLink { get; set; } = "https://via.placeholder.com/300x300";
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public Category Category { get; set; }
+        public int CategoryID { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; }
+    }
+}
