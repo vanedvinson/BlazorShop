35896dbe8b12b71c09c1d43531dd9a63fc2e8f48
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-08-28 23:04:13 +0200

    Adding Services

---
+++ BlazorShop/Server/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorShop.Shared.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Server.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        public List<Category> Categories { get; set; } = new List<Category>
+            {
+                new Category {ID=1, Name="Feder Klompe", URL="feder", Icon="book"},
+                new Category {ID=2, Name="Vazdusne Klompe", URL="vazdusne-klompe", Icon="fire"},
+                new Category {ID=3, Name="Poliuretanske Klompe", URL="pu-klompe", Icon="check"},
+            };
+        public async Task<List<Category>> GetCategories()
+        {
+            return Categories;
+        }
+    }
+}
