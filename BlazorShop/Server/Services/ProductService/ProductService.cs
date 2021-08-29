dcc7595f2955352f302d751bf779c124cbcd0ccb
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-08-29 22:59:35 +0200

    Created files for the product controller

---
+++ BlazorShop/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+    }
+}
