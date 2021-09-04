b18128ca00abae9f2e69766c5c0e83adbe0bd72c
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-09-05 00:28:06 +0200

    getting variations on product page

--- BlazorShop/Shared/Classes/Edition.cs
+++ BlazorShop/Shared/Classes/Edition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BlazorShop.Shared.Classes
@@ -10,6 +11,7 @@ namespace BlazorShop.Shared.Classes
     {
         public int ID { get; set; } 
         public string Name { get; set; }
+        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
