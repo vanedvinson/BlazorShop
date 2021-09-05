00bcea0914fc6b592f5df1910c45d79adefd2ff1
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-09-05 11:19:37 +0200

    Class for  variations and relation

---
+++ BlazorShop/Shared/Classes/Edition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Shared.Classes
+{
+    public class Edition
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BlazorShop/Shared/Classes/Product.cs
+++ BlazorShop/Shared/Classes/Product.cs
@@ -13,15 +13,14 @@ namespace BlazorShop.Shared.Classes
         public string Title { get; set; }
         public string Descripton { get; set; }
         public string ImageLink { get; set; } = "https://via.placeholder.com/300x300";
-        [Column(TypeName = "decimal(18,2)")] //two characters after the point
-        public decimal Price { get; set; }
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal OriginalPrice { get; set; }
+        
         public bool IsPublic { get; set; }
         public bool IsDeleted { get; set; }
         public Category Category { get; set; }
         public int CategoryID { get; set; }
         public DateTime? DateCreated { get; set; } = DateTime.Now;
         public DateTime? DateUpdated { get; set; }
+
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
