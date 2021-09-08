f7b515c0e296def54307681d3c7b6af418dd6de1
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-09-08 14:08:15 +0200

    Created cart service

---
+++ BlazorShop/Client/Services/CartService/CartService.cs
@@ -0,0 +1,44 @@
+﻿using Blazored.LocalStorage;
+using Blazored.Toast.Services;
+using BlazorShop.Client.Services.ProductService;
+using BlazorShop.Shared.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly IToastService _toastService;
+        private readonly IProductService _productService;
+
+        public event Action OnChange;
+
+        public CartService(ILocalStorageService localStorage, IToastService toastService, IProductService productService)
+        {
+            _localStorage = localStorage;
+            _toastService = toastService;
+            _productService = productService;
+        }
+
+        public async Task AddToCart(ProductVariant productVariant)
+        {
+            var cart = await _localStorage.GetItemAsync<List<ProductVariant>>("cart");
+            if (cart == null)
+            {
+                cart = new List<ProductVariant>();
+            }
+
+            cart.Add(productVariant);
+            await _localStorage.SetItemAsync("cart", cart);
+
+            var product = await _productService.GetProduct(productVariant.ProductId);
+            _toastService.ShowSuccess(product.Title, "Added to cart:");
+
+            OnChange.Invoke();
+        }
+    }
+}
---
+++ BlazorShop/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorShop.Shared.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Client.Services.CartService
+{
+     public interface ICartService
+     {
+        event Action OnChange;
+
+        Task AddToCart(ProductVariant productVariant);
+     }
+}
