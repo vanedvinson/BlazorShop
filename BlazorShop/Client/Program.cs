81fba1b3ebfdf0feffddbb6d0db523cd13a51b63
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-09-11 15:09:41 +0200

    Added services

--- BlazorShop/Client/Program.cs
+++ BlazorShop/Client/Program.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using Blazored.Toast;
 using BlazorShop.Client.Services.CartService;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorShop.Client
 {
@@ -29,6 +30,10 @@ namespace BlazorShop.Client
 
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddBlazoredToast();
+
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 
             await builder.Build().RunAsync();
